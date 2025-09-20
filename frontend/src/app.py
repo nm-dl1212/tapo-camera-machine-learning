9f3f5adc51616b352a52102e94e358f2c1a1fea1
nmatsumura <n.matsumura2022@gmail.com>
2025-09-20 16:12:59 +0900

    mod: docker-compose setup

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import time
 
-BACKEND_URL = "http://localhost:8000"  # FastAPI サーバーURL
+BACKEND_URL = "http://backend:8000"  # FastAPI サーバーURL
 
 st.set_page_config(page_title="Camera Control", layout="wide")
 
